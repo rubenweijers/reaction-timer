4df80519ac01656af481823251938c710cd2eabc
ruben <rubenweijers29@gmail.com>
2024-05-08 16:53:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"></Results>
+
+<!-- <p v-if="showResults">Reaction time: {{ score }} ms</p> -->
+</template>
+
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+
+export default {
+  name: 'App',
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 3000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  }
+}
+</script>
+
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,37 @@
+<template>
+    <p>Reaction time - {{ score }}</p>
+    <p class="rank">Your rank: {{ rank }}</p>
+</template>
+
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Max Verstappen'
+        } else if (this.score < 400) {
+            this.rank = 'Medium'
+        } else {
+            this.rank = 'Zzz'
+        }
+    }
+
+}
+
+</script>
+
+
+<style>
+.rank {
+    font-size: 1.4em;
+    color: #5018a3;
+    font-weight: bold;
+}
+
+</style>
